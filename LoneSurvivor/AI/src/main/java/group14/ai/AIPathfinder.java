329a876353a8c2c0df101976e2d6c814f404e5fe
Frederikke Lan <fjoer20@student.sdu.dk>
2022-04-25 19:36:35 +0200

    AI

--- LoneSurvivor/AI/src/main/java/group14/ai/AIPathfinder.java
+++ LoneSurvivor/AI/src/main/java/group14/ai/AIPathfinder.java
@@ -7,7 +7,9 @@ package group14.ai;
 import group14.common.game.GameData;
 import group14.common.game.World;
 import group14.common.gameobjects.Entity;
+import group14.common.gameobjects.components.PlayerPart;
 import group14.common.gameobjects.components.Position;
+import group14.common.services.ICollisionChecker;
 import group14.common.services.IPathfinder;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
@@ -21,10 +23,47 @@ import org.openide.util.lookup.ServiceProviders;
 
 public class AIPathfinder implements IPathfinder{
     
+    private AIEngine aiEngine = new AIEngine(null);
+    private RandomEngine randomEngine = new RandomEngine(null);
     
-
+    public void addCollisionChecker(ICollisionChecker collisionChecker) {
+        this.aiEngine.setCollisionEngine(collisionChecker);
+        this.randomEngine.setCollisionEngine(collisionChecker);
+    }
+    
+    public void removeCollisionChecker(ICollisionChecker collisionChecker) {
+        this.aiEngine.setCollisionEngine(null);
+        this.randomEngine.setCollisionEngine(null);
+    }
+    
     @Override
     public Position findNextPosition(Entity e, GameData gameData, World world) {
+        Position nextPosition = null;
+        
+        Entity target = getTarget(e, world);
+        if (target != null) {
+            Position targetPosition = target.getComponent(Position.class);
+            if (this.aiEngine != null) {
+                nextPosition = this.aiEngine.search(world, e, targetPosition);
+            }
+        }
+
+        if (randomEngine != null && nextPosition == null) {
+            nextPosition = randomEngine.randomMove(world, e);
+        }
+
+        return nextPosition;
+    }
+    
+    private Entity getTarget(Entity e, World world) {
+        for (Entity entity : world.getEntities()) {
+            PlayerPart playerPart = entity.getComponent(PlayerPart.class);
+            if (entity != e && playerPart != null) {
+                return entity;
+            }
+        }
+
+        return null;
     }
     
 }

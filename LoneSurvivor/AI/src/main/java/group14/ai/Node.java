c3ce333abd2e40ddcdf8235d4f1c57856b1423ca
Dilara <dcele20@student.sdu.dk>
2022-04-26 13:32:14 +0200

    AI module added

---
+++ LoneSurvivor/AI/src/main/java/group14/ai/Node.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group14.ai;
+
+/**
+ *
+ * @author Dilara
+ */
+public class Node {
+    
+}

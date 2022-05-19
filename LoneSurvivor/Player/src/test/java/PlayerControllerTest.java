0f1310bd2c1c33757eb3179b5729922350af6cef
Frederikke Lan <fjoer20@student.sdu.dk>
2022-05-19 11:59:35 +0200

    Test

--- LoneSurvivor/Player/src/test/java/PlayerControllerTest.java
+++ LoneSurvivor/Player/src/test/java/PlayerControllerTest.java
@@ -14,6 +14,7 @@ import group14.common.gameobjects.Player;
 import group14.common.gameobjects.components.Movement;
 import group14.common.gameobjects.components.Position;
 import group14.player.PlayerController;
+import java.awt.event.KeyEvent;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -42,9 +43,10 @@ public class PlayerControllerTest {
         world = new World();
         gameData = new GameData();
         
+        
         gameData.getInput().isDown(0);
-        gameData.setSceneHeight(200);
-        gameData.setSceneWidth(100);
+        gameData.setSceneWidth(200);
+        gameData.setSceneHeight(300);
         gameData.setDeltaTime(1);
         
         float speed = 300;
@@ -53,9 +55,11 @@ public class PlayerControllerTest {
         
         float heigth = 13 * 3;
         float width = 12 * 3;
+        float x = 100;
+        float y = 100;
         
         player.addComponent(new Movement(speed));
-        player.addComponent(new Position(gameData.getSceneWidth()/2, gameData.getSceneHeight()/2, 3.1415f / 2));
+        player.addComponent(new Position(x,y, 3.1415f / 2));
    
         world.addEntity(player);
     
@@ -66,16 +70,51 @@ public class PlayerControllerTest {
         
         Player player = (Player) world.getEntities(Player.class).get(0);
         Position position = player.getComponent(Position.class);
-       
+        Movement movement = player.getComponent(Movement.class);
+        
         float x = position.getX();
         float y = position.getY();
-        float radians = position.getRadians();
         
-        
+        //playerController.update(gameData, world);
         
         position = player.getComponent(Position.class);
-        Assert.assertNotEquals(x,position.getX());
-        Assert.assertNotEquals(y,position.getY());
+        
+        Assert.assertNotEquals(x, (float) gameData.getSceneWidth()/2);
+        Assert.assertNotEquals(y, (float) gameData.getSceneHeight()/2);
+       
+        
+//        movement.setUp(gameData.getInput().isDown(UP));
+        
+//        for (int i = 0; i < 1000; i++) {
+//            movement.setUp(true); 
+//        }
+        
+//        movement.setUp(true);
+        
+        
+//        for (int i = 0; i < 10; i++) {
+//            movement.KeyPress(KeyEvent.VK_UP);
+//            
+//        }
+//        
+//        if (gameData.getDeltaTime() != 1000) {
+//            movement.setUp(true);
+//            gameData.setDeltaTime(gameData.getDeltaTime() + 1);
+//            System.out.println("test if");
+//        }
+//        
+        
+
+
+        
+        
+
+//        System.out.println(x);
+//        System.out.println((float)gameData.getSceneWidth()/2);
+        
+        
+        
+        
         
         
     }

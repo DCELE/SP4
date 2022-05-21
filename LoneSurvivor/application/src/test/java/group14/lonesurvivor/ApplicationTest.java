53868465b62bc0362d4609ec240fbc9a61d77a9e
Muhamad <munad20@student.sdu.dk>
2022-05-21 15:51:43 +0200

    test

--- LoneSurvivor/application/src/test/java/group14/lonesurvivor/ApplicationTest.java
+++ LoneSurvivor/application/src/test/java/group14/lonesurvivor/ApplicationTest.java
@@ -1,9 +1,9 @@
 package group14.lonesurvivor;
 
-import static com.badlogic.gdx.utils.TimeUtils.millis;
+
+import group14.common.services.IPlugin;
 import group14.common.services.IUpdate;
 import java.io.IOException;
-import group14.common.services.IPlugin;
 
 import static java.nio.file.Files.copy;
 import static java.nio.file.Paths.get;
@@ -18,8 +18,8 @@ import org.netbeans.junit.NbTestCase;
 import org.openide.util.Lookup;
 
 public class ApplicationTest extends NbTestCase {
-    
-    
+
+    // TODO: Put your own paths here...
     private static final String ADD_ENEMY_UPDATES_FILE = "/Users/muhamadsdu/Desktop/SP4/LoneSurvivor/application/src/test/resources/enemy/updates.xml";
     private static final String REM_ENEMY_UPDATES_FILE = "/Users/muhamadsdu/Desktop/SP4/LoneSurvivor/application/src/test/resources/remenemy/updates.xml";
     private static final String UPDATES_FILE = "/Users/muhamadsdu/Desktop/SP4/LoneSurvivor/netbeans_site/updates.xml";
@@ -29,7 +29,7 @@ public class ApplicationTest extends NbTestCase {
                 gui(false).
                 failOnMessage(Level.WARNING). // works at least in RELEASE71
                 failOnException(Level.INFO).
-                enableClasspathModules(false). 
+                enableClasspathModules(false).
                 clusters(".*").
                 suite(); // RELEASE71+, else use NbModuleSuite.create(NbModuleSuite.createConfiguration(...))
     }
@@ -38,49 +38,57 @@ public class ApplicationTest extends NbTestCase {
         super(n);
     }
 
-public void testApplication() throws InterruptedException, IOException {
-
-        // SETUP
-        List<IUpdate> update = new CopyOnWriteArrayList<>();
-        List<IPlugin> plugin = new CopyOnWriteArrayList<>();
-
-        waitForUpdate(update, plugin);
+    public void testApplication() throws InterruptedException, IOException {
         
+        System.out.println("hej" + ADD_ENEMY_UPDATES_FILE);
+        System.out.println("hej" + REM_ENEMY_UPDATES_FILE);
+        System.out.println("hej" + UPDATES_FILE);
+        // SETUP
+        List<IUpdate> processors = new CopyOnWriteArrayList<>();
+        List<IPlugin> plugins = new CopyOnWriteArrayList<>();
+
+       
+         waitForUpdate(processors, plugins, 1000);
+
         // PRE ASSERTS
         //Size should be 0 because no modules are installed.
-        assertEquals("No plugins", 0, update.size());
-        assertEquals("No processors", 0, plugin.size());
+       
+        
 
+        // PRE ASSERTS
+        //Size should be 0 because no modules are installed.
+      
         // TEST: Load Enemy via UC
         copy(get(ADD_ENEMY_UPDATES_FILE), get(UPDATES_FILE), REPLACE_EXISTING);
+           
+        waitForUpdate(processors, plugins, 10000);
 
-        waitForUpdate(update, plugin);
-
+        
+        
         // ASSERTS: Enemy loaded
-        assertEquals("One plugins", 4, plugin.size());
-        assertEquals("One processors", 4, update.size());
-
+        assertEquals("One plugins", 4, plugins.size());
+        assertEquals("One processors", 4, processors.size());
+   
         // TEST: Unload Enemy via UC
         copy(get(REM_ENEMY_UPDATES_FILE), get(UPDATES_FILE), REPLACE_EXISTING);
 
-        waitForUpdate(update, plugin);
-
+         waitForUpdate(processors, plugins, 10000);
         // ASSERTS: Enemy unloaded
-        assertEquals("No plugins", 4, plugin.size());
-        assertEquals("No processors", 4, update.size());
+        assertEquals("No plugins", 3, plugins.size());
+        assertEquals("No processors", 3, processors.size());
 
         copy(get(ADD_ENEMY_UPDATES_FILE), get(UPDATES_FILE), REPLACE_EXISTING);
     }
 
-    private void waitForUpdate(List<IUpdate> update, List<IPlugin> plugin) throws InterruptedException {
+    private void waitForUpdate(List<IUpdate> processors ,List<IPlugin> plugins, long millis) throws InterruptedException {
         // Needs time for silentUpdater to install all modules
-        Thread.sleep(10000);
+        Thread.sleep(millis);
 
-        update.clear();
-        update.addAll(Lookup.getDefault().lookupAll(IUpdate.class));
+        processors.clear();
+        processors.addAll(Lookup.getDefault().lookupAll(IUpdate.class));
 
-        plugin.clear();
-        plugin.addAll(Lookup.getDefault().lookupAll(IPlugin.class));
+        plugins.clear();
+        plugins.addAll(Lookup.getDefault().lookupAll(IPlugin.class));
     }
 
 }
